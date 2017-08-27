79c93732f6a3e27624071b04fd6295475accf890
Guillaume <bonjour@giom.fr>
2017-08-27 18:41:36 +0200

    header

--- themes/custom/curious/js-source/custom.js
+++ themes/custom/curious/js-source/custom.js
@@ -45,28 +45,34 @@
   Drupal.behaviors.scrollTo = {
     attach: function (context, settings) {
       
-        // Add smooth scrolling to all links
-        $("a").on('click', function(event) {
+      var
+        interval = 2000,
+        intervalInit = 2000,
+        fadeInSpeed = 300,
+        elms = [
+        '#block-curious-testtext h2',
+        '#block-curious-testtext > div',
+        '.navbar-default'
+        ],
+        count = 0;
+      
+      for (var i = elms.length - 1; i >= 0; i--) {
+        $(elms[i]).hide();
+      }
+      
+      function myFunc() {
+        
+        if (count <= elms.length) {
+          var elm = $(elms[count]);
+          elm.fadeIn(fadeInSpeed);
+          count += 1;
+          setTimeout(myFunc, interval);
+        }
 
-          // Make sure this.hash has a value before overriding default behavior
-          if (this.hash !== "") {
-            // Prevent default anchor click behavior
-            event.preventDefault();
-
-            // Store hash
-            var hash = this.hash;
-
-            // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-            $('html, body').animate({
-              scrollTop: $(hash).offset().top
-            }, 800, function(){
-         
-              // Add hash (#) to URL when done scrolling (default click behavior)
-              window.location.hash = hash;
-            });
-          } // End if
-        });
+      };
+      
+      setTimeout(myFunc, intervalInit);
+       
       
     }
   };
